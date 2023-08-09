c84272395f044d65c9b9fb44d4a088ca723868d3
zeehyo <zeehyo2218@gmail.com>
2023-08-09 21:41:42 +0900

    Initial commit

---
+++ src/main/java/com/example/tutorial_springboot/TutorialSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorial_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tutorial_springboot/TutorialSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorial_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
